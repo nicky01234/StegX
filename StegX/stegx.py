1e610ab7ca2142122d7227722dcdd21709bbd78a
K NEERAJJ CHOWDARY <neerajjchowdary46@gmail.com>
2026-03-01 22:41:54 +0530

    Refactor StegX menu and integrate modules

--- StegX/stegx.py
+++ StegX/stegx.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from colorama import Fore, Style
+from modules import image, audio, text
 
 VERSION = "1.0.0"
 
@@ -10,11 +11,15 @@ def banner():
         print(Style.RESET_ALL)
 
 def main_menu():
-    print("1) Image Steganography")
+    print("\n1) Image Steganography")
     print("2) Audio Steganography")
-    print("3) Video Steganography")
-    print("4) Text Steganography")
+    print("3) Text Steganography")
     print("0) Exit")
+    return input("\nSelect option: ")
+
+def sub_menu():
+    print("\n1) Embed")
+    print("2) Extract")
     return input("Select option: ")
 
 def main():
@@ -24,16 +29,30 @@ def main():
         choice = main_menu()
 
         if choice == "1":
-            print("Image module selected")
+            action = sub_menu()
+            if action == "1":
+                image.embed_image()
+            elif action == "2":
+                image.extract_image()
+
         elif choice == "2":
-            print("Audio module selected")
+            action = sub_menu()
+            if action == "1":
+                audio.embed_audio()
+            elif action == "2":
+                audio.extract_audio()
+
         elif choice == "3":
-            print("Video module selected")
-        elif choice == "4":
-            print("Text module selected")
+            action = sub_menu()
+            if action == "1":
+                text.embed_text()
+            elif action == "2":
+                text.extract_text()
+
         elif choice == "0":
             print("Exiting StegX...")
             break
+
         else:
             print("Invalid option")
 
